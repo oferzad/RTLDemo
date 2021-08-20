9c2cbe2679e288b2ee7e838def3c4e6157c854c2
Ofer Zadikario <ofer.zadikario@gmail.com>
2021-08-20 12:12:10 +0300

    Add project files.

---
+++ RTLDemo/RTLDemo/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RTLDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LTRForm());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RTLForm());
+        }
+    }
+}
